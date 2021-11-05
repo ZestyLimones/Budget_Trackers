7e6e62cd3d35ed34eecd822ad65ce0d2510cc8bd
Micheal Limones <micheal@zestylimones.com>
2021-11-05 17:10:58 -0600

    removed comments from service workers file.

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,21 +10,14 @@ const FILES_TO_CACHE = [
   'manifest.json',
 ];
 
-// install
 self.addEventListener('install', function (evt) {
-  // pre cache image data
-
-  // pre cache all static assets
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 
-  // tell the browser to activate this service worker immediately once it
-  // has finished installing
   self.skipWaiting();
 });
 
-// activate
 self.addEventListener('activate', function (evt) {
   evt.waitUntil(
     caches.keys().then((keyList) => {
@@ -42,7 +35,6 @@ self.addEventListener('activate', function (evt) {
   self.clients.claim();
 });
 
-// fetch
 self.addEventListener('fetch', function (evt) {
   if (evt.request.url.includes('/api/')) {
     evt.respondWith(
@@ -51,7 +43,6 @@ self.addEventListener('fetch', function (evt) {
         .then((cache) => {
           return fetch(evt.request)
             .then((response) => {
-              // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
@@ -59,7 +50,6 @@ self.addEventListener('fetch', function (evt) {
               return response;
             })
             .catch((err) => {
-              // Network request failed, try to get it from the cache.
               return cache.match(evt.request);
             });
         })
